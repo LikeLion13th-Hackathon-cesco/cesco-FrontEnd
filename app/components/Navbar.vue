a1aac71512a45e508bd8cbab20b32905cfc2a7ec
YH <imrubi5555@gmail.com>
2025-08-20 23:44:55 +0900

    ui(navbar): 셋방살이 로고 교체

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,14 +1,15 @@
 <template>
   <nav
-    class="sticky top-0 z-[999] flex w-full items-center justify-center gap-[70px] bg-background px-[80px] py-[40px]"
+    class="sticky top-0 z-[999] flex w-full items-center justify-center bg-background px-[80px] py-[40px]"
   >
     <!-- 로고 -->
-    <NuxtLink to="/">
-      <span class="font-mono text-2xl font-light tracking-wide">Logo</span>
+    <NuxtLink to="/" class="inline-flex items-center gap-[2px]">
+      <LogoOutline :font-controlled="false" filled="false" class="h-[35px] w-[35px]" />
+      <Setbangsari :font-controlled="false" filled="false" class="h-[26px]" />
     </NuxtLink>
 
     <!-- 메뉴 -->
-    <div class="flex items-center gap-[32px]">
+    <div class="ml-[35px] flex items-center gap-[32px]">
       <NuxtLink
         to="/community"
         :class="[
@@ -61,7 +62,7 @@
 
     <!-- 검색바 -->
     <div
-      class="flex items-center gap-[12px] rounded-full border-[1.5px] border-gray-b4 px-[30px] py-[15px]"
+      class="ml-[70px] flex items-center gap-[12px] rounded-full border-[1.5px] border-gray-b4 px-[30px] py-[15px]"
     >
       <SearchIcon class="h-[20px] w-[20px]" filled="false" />
       <input
@@ -75,6 +76,8 @@
 
 <script setup>
 import SearchIcon from "~/assets/icon/search.svg";
+import LogoOutline from "~/assets/icon/logo-outline.svg";
+import Setbangsari from "~/assets/icon/setbangsari.svg";
 
 // PLUS 구독중 여부
 const isPlus = ref(true);

e0dcb281eed343a426dff7c0ed21316ea956a360
Daeuni <ekdmsjj@gmail.com>
2025-08-24 19:48:11 +0900

    UI(#37) 퀴즈 페이지 UI

---
+++ app/pages/quiz/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="mb-[100px] flex flex-col items-center justify-center">
+    <div class="text-[40px] font-[700] text-gray-1a">
+      다음 중 전입신고에 대한 설명으로 옳지 않은 것은?
+    </div>
+    <div class="mt-[50px] flex flex-col gap-[40px]">
+      <div
+        class="flex items-center justify-center rounded-2xl border-[1.50px] border-zinc-400 bg-white px-[175px] py-[27px] text-[28px] font-[500] hover:border-[3px] hover:border-primary hover:bg-secondary"
+      >
+        보증금 보호를 위해 전입신고는 필수다.
+      </div>
+      <div
+        class="flex items-center justify-center rounded-2xl border-[1.50px] border-zinc-400 bg-white px-[175px] py-[27px] text-[28px] font-[500] hover:border-[3px] hover:border-primary hover:bg-secondary"
+      >
+        전입신고는 동사무소나 온라인에서 할 수 있다.
+      </div>
+      <div
+        class="flex items-center justify-center rounded-2xl border-[1.50px] border-zinc-400 bg-white px-[175px] py-[27px] text-[28px] font-[500] hover:border-[3px] hover:border-primary hover:bg-secondary"
+      >
+        전입신고를 하면 확정일자가 자동으로 부여된다.
+      </div>
+      <div
+        class="flex items-center justify-center rounded-2xl border-[1.50px] border-zinc-400 bg-white px-[175px] py-[27px] text-[28px] font-[500] hover:border-[3px] hover:border-primary hover:bg-secondary"
+      >
+        전입신고를 하면 주민등록 주소가 해당 집으로 변경된다.
+      </div>
+    </div>
+    <button
+      class="mt-[50px] flex items-center justify-center rounded-[10px] bg-[#B4B4B4] px-[182px] py-[15px] text-[32px] font-[600] text-gray-fe hover:bg-primary"
+    >
+      확인하기
+    </button>
+  </div>
+</template>

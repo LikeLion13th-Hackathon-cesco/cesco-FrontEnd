60c2cef5044d9ee84cc46346a4d88917842b984b
Daeuni <ekdmsjj@gmail.com>
2025-08-25 01:02:24 +0900

    ui(#37) 퀴즈 페이지 UI 정답 오답창, 모달

---
+++ app/pages/quiz/_components/Explain.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex items-center justify-center">
+    <div class="flex flex-col items-start justify-start">
+      <slot name="result"></slot>
+      <slot name="resultExplain"></slot>
+      <div
+        class="my-[40px] h-0 w-[1003px] outline outline-[1.50px] outline-offset-[-0.75px] outline-zinc-300"
+      ></div>
+      <div class="flex flex-col items-start">
+        <div class="justify-start text-[28px] font-[600] leading-[50px] text-zinc-900">해설:</div>
+        <div class="mb-[50px] justify-start text-[28px] font-[600] leading-[50px] text-zinc-900">
+          전입신고가 되었더라도
+          <span class="text-primary">확정일자는 별도로</span>
+          받아야 합니다.
+        </div>
+        <div class="justify-start text-[28px] font-[400] leading-[50px] text-zinc-900">
+          1. 보증금 보호를 위해 전입신고는 필수다.
+        </div>
+        <div class="justify-start text-[28px] font-[400] leading-[50px] text-zinc-900">
+          2. 전입신고는 동사무소나 온라인에서 할 수 있다.
+        </div>
+        <div class="justify-start text-[28px] font-[400] leading-[50px] text-zinc-900">
+          4. 전입신고를 하면 주민등록 주소가 해당 집으로 변경된다.
+        </div>
+        <div class="mt-[40px] flex gap-[500px]">
+          <div class="justify-start text-[28px] font-[400] leading-[50px] text-zinc-900">
+            -> 나머지 보기는 모두 맞는 사실입니다.
+          </div>
+          <button
+            class="item-center flex justify-center rounded-[10px] bg-primary px-[27px] py-[15px] text-[45px] text-white"
+            @click="handleModal"
+          >
+            ->
+          </button>
+        </div>
+      </div>
+      <div
+        v-if="isModalOpen"
+        class="fixed inset-0 z-50 flex items-center justify-center bg-zinc-900/40"
+      >
+        <CompleteQuiz @close="closeModal" @check-credit="handleCreditCheck" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CompleteQuiz from "../_modals/completeQuiz.vue";
+
+const isModalOpen = ref(false);
+const handleModal = () => {
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = !isModalOpen.value;
+};
+const handleCreditCheck = () => {
+  navigateTo("/my");
+  console.log("크레딧 페이지로 이동");
+};
+</script>
---
+++ app/pages/quiz/_modals/CompleteQuiz.vue
@@ -0,0 +1,52 @@
+<template>
+  <div
+    class="flex h-[487px] w-[676px] flex-col items-center rounded-[15px] bg-white px-[100px] py-[60px]"
+  >
+    <CheckIcon filled="false" :font-controlled="false" class="h-[95px] w-[95px]"></CheckIcon>
+    <div
+      class="mb-[20px] mt-[34px] justify-start font-['Pretendard'] text-3xl font-semibold text-zinc-900"
+    >
+      문제 풀이가 완료되었습니다
+    </div>
+    <div class="mb-[30px] justify-start text-center">
+      <span class="font-['Pretendard'] text-2xl font-normal leading-6 text-neutral-400">
+        참여 감사합니다.
+        <br />
+        감사 크레딧
+      </span>
+      <span class="font-['Pretendard'] text-2xl font-semibold leading-6 text-blue-500">500P</span>
+      <span class="font-['Pretendard'] text-2xl font-normal leading-6 text-neutral-400">
+        가 적립되었습니다.
+      </span>
+    </div>
+    <div class="flex gap-10">
+      <button
+        class="flex h-16 w-56 items-center justify-center rounded-[10px] bg-[#B4B4B4] text-[26px] font-semibold text-white"
+        @click="handleClose"
+      >
+        닫기
+      </button>
+      <button
+        class="flex h-16 w-56 items-center justify-center rounded-[10px] bg-blue-500 text-[26px] font-semibold text-white"
+        @click="handleCreditCheck"
+      >
+        크레딧 확인
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import CheckIcon from "~/assets/icon/achievement-circle-filled.svg";
+
+const emit = defineEmits(["close", "checkCredit"]);
+
+const handleClose = () => {
+  emit("close");
+};
+
+const handleCreditCheck = () => {
+  emit("checkCredit");
+  emit("close");
+};
+</script>
